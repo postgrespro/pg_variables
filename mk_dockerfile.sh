493aa8df1c4ce008ba499116f8e609d960cbb131
Dmitry Ivanov <ivadmi5@gmail.com>
2018-04-17 16:00:19 +0300

    implement several levels in run_tests.sh

--- mk_dockerfile.sh
+++ mk_dockerfile.sh
@@ -1,2 +1,2 @@
 set -eu
-sed -e 's/${PG_VERSION}/'${PG_VERSION}/g Dockerfile.tmpl > Dockerfile
+sed -e 's/${PG_VERSION}/'${PG_VERSION}/g -e 's/${LEVEL}/'${LEVEL}/g Dockerfile.tmpl > Dockerfile
--- run_tests.sh
+++ run_tests.sh
@@ -3,29 +3,84 @@
 # Copyright (c) 2018, Postgres Professional
 
 
+# provide a decent default level
+if [ -z ${LEVEL+x} ]; then
+	LEVEL=scan-build
+fi
+
 set -ux
 
-
 status=0
+
 
 # show pg_config just in case
 pg_config
 
-# perform static analyzis
-scan-build --status-bugs make USE_PGXS=1 || status=$?
 
-# something's wrong, exit now!
-if [ $status -ne 0 ]; then exit 1; fi
+# perform code checks if asked to
+if [ "$LEVEL" = "scan-build" ]; then
 
-# don't forget to "make clean"
-make USE_PGXS=1 clean
+	# perform static analyzis
+	scan-build --status-bugs make USE_PGXS=1 || status=$?
 
-# initialize database
-initdb -D $PGDATA
+	# something's wrong, exit now!
+	if [ $status -ne 0 ]; then exit 1; fi
+
+	# don't forget to "make clean"
+	make USE_PGXS=1 clean
+fi
+
+# build with cassert + valgrind support
+if [ "$LEVEL" = "hardcore" ]; then
+
+	set -e
+
+	CUSTOM_PG_PATH=$PWD/pg_bin
+
+	# here PG_VERSION is provided by postgres:X-alpine docker image
+	wget -O postgresql.tar.bz2 "https://ftp.postgresql.org/pub/source/v$PG_VERSION/postgresql-$PG_VERSION.tar.bz2"
+	echo "$PG_SHA256 *postgresql.tar.bz2" | sha256sum -c -
+
+	mkdir postgresql
+
+	tar \
+		--extract \
+		--file postgresql.tar.bz2 \
+		--directory postgresql \
+		--strip-components 1
+
+	cd postgresql
+
+	# enable Valgrind support
+	sed -i.bak "s/\/* #define USE_VALGRIND *\//#define USE_VALGRIND/g" src/include/pg_config_manual.h
+
+	# enable additional options
+	eval ./configure \
+		--with-gnu-ld \
+		--enable-debug \
+		--enable-cassert \
+		--prefix=$CUSTOM_PG_PATH
+
+	# TODO: -j$(nproc)
+	make -s -j1 && make install
+
+	# override default PostgreSQL instance
+	export PATH=$CUSTOM_PG_PATH/bin:$PATH
+
+	# show pg_config path (just in case)
+	which pg_config
+
+	cd -
+
+	set +e
+fi
 
 # build and install extension (using PG_CPPFLAGS and SHLIB_LINK for gcov)
 make USE_PGXS=1 PG_CPPFLAGS="-coverage" SHLIB_LINK="-coverage"
 make USE_PGXS=1 install
+
+# initialize database
+initdb -D $PGDATA
 
 # restart cluster 'test'
 echo "port = 55435" >> $PGDATA/postgresql.conf
